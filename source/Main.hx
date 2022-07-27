6de68014a610840e37475d71f51f62968cef8e6c
Phzinphycho12 <106931352+Phzinphycho12@users.noreply.github.com>
2022-07-26 21:00:05 -0300

    update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -37,8 +37,6 @@ class Main extends Sprite
 
 	public function new()
 	{
-                SUtil.uncaughtErrorHandler();
-
 		super();
 
 		if (stage != null)
@@ -75,13 +73,20 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-                SUtil.check();
+		#if cpp
+	//	initialState = TitleState;
+		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
+		#else
+		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
+		#end
 
 		addChild(game);
+
 
 		fpsCounter = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsCounter);
 		toggleFPS(FlxG.save.data.fps);
+
 
 	}
 
