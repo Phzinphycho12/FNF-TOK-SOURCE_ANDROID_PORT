dbb4350213c98dce7c07dd0e2bd88fe85a3a2ae6
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-07-26 21:14:26 -0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -131,7 +131,7 @@ class TitleState extends MusicBeatState
 		
 		PlayerSettings.init();
 
-		#if windows
+		#if desktop
 		DiscordClient.initialize();
 
 		Application.current.onExit.add (function (exitCode) {
